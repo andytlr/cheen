ae68114e77ce1c9e3b98de4da9a98d0c64a190eb
Andy Taylor <andytlr@gmail.com>
2012-10-18 12:37:42 +1100

    First upload.

---
+++ js/scripts.js
@@ -0,0 +1,199 @@
+// Cycle body classes jQuery script
+
+$(function() {
+  var $target = $("body");
+  var classes = ['image1', 'image2', 'image3'];
+  var current = 0;
+
+  setInterval(function() {
+    $target.removeClass(classes[current]);
+    current = (current+1)%classes.length;
+    $target.addClass(classes[current]);
+  }, 10000); // Timer in ms
+});
+
+// Hide URL Bar for iOS
+// http://davidwalsh.name/hide-address-bar
+
+window.addEventListener("load",function() {
+  // Set a timeout...
+  setTimeout(function(){
+    // Hide the address bar!
+    window.scrollTo(0, 1);
+  }, 0);
+});
+
+/**
+ * jQuery-Plugin "preloadCssImages"
+ * by Scott Jehl, scott@filamentgroup.com
+ * http://www.filamentgroup.com
+ * reference article: http://www.filamentgroup.com/lab/update_automatically_preload_images_from_css_with_jquery/
+ * demo page: http://www.filamentgroup.com/examples/preloadImages/index_v2.php
+ * 
+ * Copyright (c) 2008 Filament Group, Inc
+ * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) and GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
+ *
+ * Version: 5.0, 10.31.2008
+ * Changelog:
+ * 	02.20.2008 initial Version 1.0
+ *    06.04.2008 Version 2.0 : removed need for any passed arguments. Images load from any and all directories.
+ *    06.21.2008 Version 3.0 : Added options for loading status. Fixed IE abs image path bug (thanks Sam Pohlenz).
+ *    07.24.2008 Version 4.0 : Added support for @imported CSS (credit: http://marcarea.com/). Fixed support in Opera as well. 
+ *    10.31.2008 Version: 5.0 : Many feature and performance enhancements from trixta
+ * --------------------------------------------------------------------
+ */
+
+;jQuery.preloadCssImages = function(settings){
+	settings = jQuery.extend({
+		statusTextEl: null,
+		statusBarEl: null,
+		errorDelay: 999, // handles 404-Errors in IE
+		simultaneousCacheLoading: 2
+	}, settings);
+	var allImgs = [],
+		loaded = 0,
+		imgUrls = [],
+		thisSheetRules,	
+		errorTimer;
+
+	function onImgComplete(){
+		clearTimeout(errorTimer);
+		if (imgUrls && imgUrls.length && imgUrls[loaded]) {
+			loaded++;
+			if (settings.statusTextEl) {
+				var nowloading = (imgUrls[loaded]) ? 
+					'Now Loading: <span>' + imgUrls[loaded].split('/')[imgUrls[loaded].split('/').length - 1] : 
+					'Loading complete'; // wrong status-text bug fixed
+				jQuery(settings.statusTextEl).html('<span class="numLoaded">' + loaded + '</span> of <span class="numTotal">' + imgUrls.length + '</span> loaded (<span class="percentLoaded">' + (loaded / imgUrls.length * 100).toFixed(0) + '%</span>) <span class="currentImg">' + nowloading + '</span></span>');
+			}
+			if (settings.statusBarEl) {
+				var barWidth = jQuery(settings.statusBarEl).width();
+				jQuery(settings.statusBarEl).css('background-position', -(barWidth - (barWidth * loaded / imgUrls.length).toFixed(0)) + 'px 50%');
+			}
+			loadImgs();
+		}
+	}
+
+	function loadImgs(){
+		//only load 1 image at the same time / most browsers can only handle 2 http requests, 1 should remain for user-interaction (Ajax, other images, normal page requests...)
+		// otherwise set simultaneousCacheLoading to a higher number for simultaneous downloads
+		if(imgUrls && imgUrls.length && imgUrls[loaded]){
+			var img = new Image(); //new img obj
+			img.src = imgUrls[loaded];	//set src either absolute or rel to css dir
+			if(!img.complete){
+				jQuery(img).bind('error load onreadystatechange', onImgComplete);
+			} else {
+				onImgComplete();
+			}
+			errorTimer = setTimeout(onImgComplete, settings.errorDelay); // handles 404-Errors in IE
+		}
+	}
+
+	function parseCSS(sheets, urls) {
+		var w3cImport = false,
+			imported = [],
+			importedSrc = [],
+			baseURL;
+		var sheetIndex = sheets.length;
+		while(sheetIndex--){//loop through each stylesheet
+
+			var cssPile = '';//create large string of all css rules in sheet
+
+			if(urls && urls[sheetIndex]){
+				baseURL = urls[sheetIndex];
+			} else {
+				var csshref = (sheets[sheetIndex].href) ? sheets[sheetIndex].href : 'window.location.href';
+				var baseURLarr = csshref.split('/');//split href at / to make array
+				baseURLarr.pop();//remove file path from baseURL array
+				baseURL = baseURLarr.join('/');//create base url for the images in this sheet (css file's dir)
+				if (baseURL) {
+					baseURL += '/'; //tack on a / if needed
+				}
+			}
+			if(sheets[sheetIndex].cssRules || sheets[sheetIndex].rules){
+				thisSheetRules = (sheets[sheetIndex].cssRules) ? //->>> http://www.quirksmode.org/dom/w3c_css.html
+					sheets[sheetIndex].cssRules : //w3
+					sheets[sheetIndex].rules; //ie 
+				var ruleIndex = thisSheetRules.length;
+				while(ruleIndex--){
+					if(thisSheetRules[ruleIndex].style && thisSheetRules[ruleIndex].style.cssText){
+						var text = thisSheetRules[ruleIndex].style.cssText;
+						if(text.toLowerCase().indexOf('url') != -1){ // only add rules to the string if you can assume, to find an image, speed improvement
+							cssPile += text; // thisSheetRules[ruleIndex].style.cssText instead of thisSheetRules[ruleIndex].cssText is a huge speed improvement
+						}
+					} else if(thisSheetRules[ruleIndex].styleSheet) {
+						imported.push(thisSheetRules[ruleIndex].styleSheet);
+						w3cImport = true;
+					}
+
+				}
+			}
+			//parse cssPile for image urls
+			var tmpImage = cssPile.match(/[^\("]+\.(gif|jpg|jpeg|png)/g);//reg ex to get a string of between a "(" and a ".filename" / '"' for opera-bugfix
+			if(tmpImage){
+				var i = tmpImage.length;
+				while(i--){ // handle baseUrl here for multiple stylesheets in different folders bug
+					var imgSrc = (tmpImage[i].charAt(0) == '/' || tmpImage[i].match('://')) ? // protocol-bug fixed
+						tmpImage[i] : 
+						baseURL + tmpImage[i];
+
+					if(jQuery.inArray(imgSrc, imgUrls) == -1){
+						imgUrls.push(imgSrc);
+					}
+				}
+			}
+
+			if(!w3cImport && sheets[sheetIndex].imports && sheets[sheetIndex].imports.length) {
+				for(var iImport = 0, importLen = sheets[sheetIndex].imports.length; iImport < importLen; iImport++){
+					var iHref = sheets[sheetIndex].imports[iImport].href;
+					iHref = iHref.split('/');
+					iHref.pop();
+					iHref = iHref.join('/');
+					if (iHref) {
+						iHref += '/'; //tack on a / if needed
+					}
+					var iSrc = (iHref.charAt(0) == '/' || iHref.match('://')) ? // protocol-bug fixed
+						iHref : 
+						baseURL + iHref;
+
+					importedSrc.push(iSrc);
+					imported.push(sheets[sheetIndex].imports[iImport]);
+				}
+
+
+			}
+		}//loop
+		if(imported.length){
+			parseCSS(imported, importedSrc);
+			return false;
+		}
+		var downloads = settings.simultaneousCacheLoading;
+		while( downloads--){
+			setTimeout(loadImgs, downloads);
+		}
+	}
+	parseCSS(document.styleSheets);
+	return imgUrls;
+};
+
+// Make the script above actually run
+
+$(document).ready(function(){ 
+$.preloadCssImages();
+});
+
+// Respond.js: min/max-width media query polyfill. (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs
+
+(function(e,h){e.respond={};respond.update=function(){};respond.mediaQueriesSupported=h;if(h){return}var u=e.document,r=u.documentElement,i=[],k=[],p=[],o={},g=30,f=u.getElementsByTagName("head")[0]||r,b=f.getElementsByTagName("link"),d=[],a=function(){var B=b,w=B.length,z=0,y,x,A,v;for(;z<w;z++){y=B[z],x=y.href,A=y.media,v=y.rel&&y.rel.toLowerCase()==="stylesheet";if(!!x&&v&&!o[x]){if(y.styleSheet&&y.styleSheet.rawCssText){m(y.styleSheet.rawCssText,x,A);o[x]=true}else{if(!/^([a-zA-Z]+?:(\/\/)?)/.test(x)||x.replace(RegExp.$1,"").split("/")[0]===e.location.host){d.push({href:x,media:A})}}}}t()},t=function(){if(d.length){var v=d.shift();n(v.href,function(w){m(w,v.href,v.media);o[v.href]=true;t()})}},m=function(G,v,x){var E=G.match(/@media[^\{]+\{([^\{\}]+\{[^\}\{]+\})+/gi),H=E&&E.length||0,v=v.substring(0,v.lastIndexOf("/")),w=function(I){return I.replace(/(url\()['"]?([^\/\)'"][^:\)'"]+)['"]?(\))/g,"$1"+v+"$2$3")},y=!H&&x,B=0,A,C,D,z,F;if(v.length){v+="/"}if(y){H=1}for(;B<H;B++){A=0;if(y){C=x;k.push(w(G))}else{C=E[B].match(/@media ([^\{]+)\{([\S\s]+?)$/)&&RegExp.$1;k.push(RegExp.$2&&w(RegExp.$2))}z=C.split(",");F=z.length;for(;A<F;A++){D=z[A];i.push({media:D.match(/(only\s+)?([a-zA-Z]+)(\sand)?/)&&RegExp.$2,rules:k.length-1,minw:D.match(/\(min\-width:[\s]*([\s]*[0-9]+)px[\s]*\)/)&&parseFloat(RegExp.$1),maxw:D.match(/\(max\-width:[\s]*([\s]*[0-9]+)px[\s]*\)/)&&parseFloat(RegExp.$1)})}}j()},l,q,j=function(E){var v="clientWidth",x=r[v],D=u.compatMode==="CSS1Compat"&&x||u.body[v]||x,z={},C=u.createDocumentFragment(),B=b[b.length-1],w=(new Date()).getTime();if(E&&l&&w-l<g){clearTimeout(q);q=setTimeout(j,g);return}else{l=w}for(var y in i){var F=i[y];if(!F.minw&&!F.maxw||(!F.minw||F.minw&&D>=F.minw)&&(!F.maxw||F.maxw&&D<=F.maxw)){if(!z[F.media]){z[F.media]=[]}z[F.media].push(k[F.rules])}}for(var y in p){if(p[y]&&p[y].parentNode===f){f.removeChild(p[y])}}for(var y in z){var G=u.createElement("style"),A=z[y].join("\n");G.type="text/css";G.media=y;if(G.styleSheet){G.styleSheet.cssText=A}else{G.appendChild(u.createTextNode(A))}C.appendChild(G);p.push(G)}f.insertBefore(C,B.nextSibling)},n=function(v,x){var w=c();if(!w){return}w.open("GET",v,true);w.onreadystatechange=function(){if(w.readyState!=4||w.status!=200&&w.status!=304){return}x(w.responseText)};if(w.readyState==4){return}w.send(null)},c=(function(){var v=false;try{v=new XMLHttpRequest()}catch(w){v=new ActiveXObject("Microsoft.XMLHTTP")}return function(){return v}})();a();respond.update=a;function s(){j(true)}if(e.addEventListener){e.addEventListener("resize",s,false)}else{if(e.attachEvent){e.attachEvent("onresize",s)}}})(this,(function(f){if(f.matchMedia){return true}var e,i=document,c=i.documentElement,g=c.firstElementChild||c.firstChild,h=!i.body,d=i.body||i.createElement("body"),b=i.createElement("div"),a="only all";b.id="mq-test-1";b.style.cssText="position:absolute;top:-99em";d.appendChild(b);b.innerHTML='_<style media="'+a+'"> #mq-test-1 { width: 9px; }</style>';if(h){c.insertBefore(d,g)}b.removeChild(b.firstChild);e=b.offsetWidth==9;if(h){c.removeChild(d)}else{d.removeChild(b)}return e})(this));
+
+// Google analytics
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5104693-16']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
